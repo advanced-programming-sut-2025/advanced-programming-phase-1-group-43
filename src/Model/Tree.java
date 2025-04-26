f508febc1427e1ea1669d30ee0284ffbc47ec35e
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-04-26 21:19:48 +0330

    Update Tree.java

--- src/Model/Tree.java
+++ src/Model/Tree.java
@@ -2,4 +2,11 @@ package Model;
 
 public class Tree extends Plant{
     private boolean isChoppable;
+    public static int  x;
+    public static int  y;
+    public Tree(int x, int y,boolean isChoppable) {
+        this.x = x;
+        this.y = y;
+        this.isChoppable = isChoppable;
+    }
 }
