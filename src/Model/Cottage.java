7056417b16dc644b424c5db62b10088aa43da902
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-05-15 14:12:46 +0330

    Create Cottage.java

---
+++ src/Model/Cottage.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Cottage {
+    public int FirstX;
+    public int FirstY;
+    public int SecondX;
+    public int SecondY;
+  
+    public Cottage(int firstX,int firstY,int secondX,int secondY) {
+        FirstX = firstX;
+        FirstY = firstY;
+        SecondX = secondX;
+        SecondY = secondY;
+    }
+
+}
