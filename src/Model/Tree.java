7e293f64fb09b1f3f17d15c239363906eeb306dc
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-05-15 14:19:56 +0330

    Update Tree.java

--- src/Model/Tree.java
+++ src/Model/Tree.java
@@ -1,5 +1,17 @@
 package Model;
 
-public class Tree extends Plant{
+import Model.Crops.Trees.All;
+
+public class Tree extends All {
     private boolean isChoppable;
+    public static int  x;
+    public static int  y;
+    public static String TreeType;
+    public boolean isAttacked = false;
+    public Tree(String Type,int x, int y,boolean isChoppable) {
+        this.TreeType = Type;
+        this.x = x;
+        this.y = y;
+        this.isChoppable = isChoppable;
+    }
 }
