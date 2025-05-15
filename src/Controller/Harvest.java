176ac422655f454dc0a455ba4b13d782e5afdf82
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-05-15 14:24:56 +0330

    Create Harvest.java

---
+++ src/Controller/Harvest.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import Model.App;
+import Model.Farm;
+import Model.Plant;
+
+public class Harvest {
+    public String Harvest(int x,int y){
+        Plant plantForRemove = null;
+        int IsSuccess = 0;
+        for(Plant plant: App.AllPlantInFarm){
+            if(plant.XPlanting == x && plant.YPlanting == y){
+                if(plant.Age == 1){
+                    //TODO
+                    plantForRemove = plant;
+                    IsSuccess = 1;
+                }
+            }
+        }
+        if(IsSuccess == 1){
+            App.AllPlantInFarm.remove(plantForRemove);
+            Farm.xLocations.remove(plantForRemove.XPlanting);
+            Farm.yLocations.remove(plantForRemove.YPlanting);
+            return "Harvest Success";
+        }else{
+            return "No Plant Found";
+        }
+    }
+}
