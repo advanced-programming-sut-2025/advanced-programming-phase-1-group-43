37befa30dd1afa8a6663223780b67f2abdb43a57
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-05-15 14:25:24 +0330

    Create HelpingReadingMap.java

---
+++ src/Controller/HelpingReadingMap.java
@@ -0,0 +1,17 @@
+package Controller;
+
+public class HelpingReadingMap {
+    public void printHelpingReadingMap() {
+        System.out.println("For Cottage : C");
+        System.out.println("For Lake : L");
+        System.out.println("For GreenHouse : G");
+        System.out.println("For StoneMining : M");
+        System.out.println("For Trees : T");
+        System.out.println("For Stone : S");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
