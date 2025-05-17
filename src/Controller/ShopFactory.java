64dffb22035a1d919ea9c8c6222f63d9efe901a8
moein <moinsadri2007@gmail.com>
2025-05-17 20:09:47 +0330

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Controller/FertilizeController.java
    #	src/Controller/Harvest.java
    #	src/Controller/HelpingReadingMap.java
    #	src/Controller/PlantingController.java
    #	src/Controller/PrintMap.java
    #	src/Controller/ShowPlant.java
    #	src/Controller/Watering.java
    #	src/Controller/showTreeInf.java
    #	src/Model/Cottage.java
    #	src/Model/Crops/FoaringCrops/All.java
    #	src/Model/Crops/FoaringMinerals/All.java
    #	src/Model/Crops/FoaringSeeds/All.java
    #	src/Model/Crops/FoaringTrees/All.java
    #	src/Model/Crops/MixedSeeds/All.java
    #	src/Model/Crops/Trees/All.java
    #	src/Model/Foraging.java
    #	src/Model/GiantPlant.java
    #	src/Model/Lake.java
    #	src/Model/PlantAndCrops/Amaranth.java
    #	src/Model/PlantAndCrops/AncientFruit.java
    #	src/Model/PlantAndCrops/Artichoke.java
    #	src/Model/PlantAndCrops/Beet.java
    #	src/Model/PlantAndCrops/BlueJazz.java
    #	src/Model/PlantAndCrops/Blueberry.java
    #	src/Model/PlantAndCrops/BokChoy.java
    #	src/Model/PlantAndCrops/Broccoli.java
    #	src/Model/PlantAndCrops/Carrot.java
    #	src/Model/PlantAndCrops/Cauliflower.java
    #	src/Model/PlantAndCrops/CoffeeBean.java
    #	src/Model/PlantAndCrops/Corn.java
    #	src/Model/PlantAndCrops/Cranberries.java
    #	src/Model/PlantAndCrops/Eggplant.java
    #	src/Model/PlantAndCrops/FairyRose.java
    #	src/Model/PlantAndCrops/Garlic.java
    #	src/Model/PlantAndCrops/Grape.java
    #	src/Model/PlantAndCrops/GreenBean.java
    #	src/Model/PlantAndCrops/Hops.java
    #	src/Model/PlantAndCrops/HotPepper.java
    #	src/Model/PlantAndCrops/Kale.java
    #	src/Model/PlantAndCrops/Melon.java
    #	src/Model/PlantAndCrops/Parsnip.java
    #	src/Model/PlantAndCrops/Poppy.java
    #	src/Model/PlantAndCrops/Potato.java
    #	src/Model/PlantAndCrops/Powdermelon.java
    #	src/Model/PlantAndCrops/Pumpkin.java
    #	src/Model/PlantAndCrops/Radish.java
    #	src/Model/PlantAndCrops/RedCabbage.java
    #	src/Model/PlantAndCrops/Rhubarb.java
    #	src/Model/PlantAndCrops/Starfruit.java
    #	src/Model/PlantAndCrops/Strawberry.java
    #	src/Model/PlantAndCrops/SummerSpangle.java
    #	src/Model/PlantAndCrops/SummerSquash.java
    #	src/Model/PlantAndCrops/Sunflower.java
    #	src/Model/PlantAndCrops/SweetGemBerry.java
    #	src/Model/PlantAndCrops/Tomato.java
    #	src/Model/PlantAndCrops/Tulip.java
    #	src/Model/PlantAndCrops/UnmilledRice.java
    #	src/Model/PlantAndCrops/Wheat.java
    #	src/Model/PlantAndCrops/Yam.java
    #	src/Model/StoneMining.java
    #	src/View/GameMapAndMoveView.java
    #	src/View/GameProgressView.java

--- src/Controller/ShopFactory.java
+++ src/Controller/ShopFactory.java
@@ -10,37 +10,56 @@ import java.util.HashMap;
 public class ShopFactory {
     public static Shop createBlacksmith() {
         Map<String, ShopItem> stock = new HashMap<>();
-        stock.put("Metal Bar", new ShopItem("Metal Bar", 300, new SeasonEnum[]{SeasonEnum.SPRING,SeasonEnum.SUMMER,SeasonEnum.FALL,SeasonEnum.WINTER}, 10));
+        stock.put("Metal Bar", new ShopItem("Metal Bar", 300, new SeasonEnum[]{SeasonEnum.SPRING, SeasonEnum.SUMMER, SeasonEnum.FALL, SeasonEnum.WINTER}, 10));
         stock.put("Coal", new ShopItem("Coal", 150, SeasonEnum.values(), 20));
         return new Shop("Blacksmith", 9, 16, stock);
     }
+
     public static Shop createJojaMart() {
         Map<String, ShopItem> stock = new HashMap<>();
-        stock.put("Wheat Seeds", new ShopItem("Wheat Seeds", 30, new SeasonEnum[]{SeasonEnum.SPRING,SeasonEnum.SUMMER}, 50));
-        stock.put("Corn Seeds", new ShopItem("Corn Seeds", 150, new SeasonEnum[]{SeasonEnum.SUMMER,SeasonEnum.FALL}, 30));
+        stock.put("Wheat Seeds", new ShopItem("Wheat Seeds", 30, new SeasonEnum[]{SeasonEnum.SPRING, SeasonEnum.SUMMER}, 50));
+        stock.put("Corn Seeds", new ShopItem("Corn Seeds", 150, new SeasonEnum[]{SeasonEnum.SUMMER, SeasonEnum.FALL}, 30));
+        stock.put("Fertilizer", new ShopItem("Fertilizer", 100, SeasonEnum.values(), 40));
         return new Shop("JojaMart", 9, 23, stock);
     }
+
     public static Shop createPierre() {
         Map<String, ShopItem> stock = new HashMap<>();
         stock.put("Parsnip Seeds", new ShopItem("Parsnip Seeds", 20, new SeasonEnum[]{SeasonEnum.SPRING}, 100));
         stock.put("Potato Seeds", new ShopItem("Potato Seeds", 50, new SeasonEnum[]{SeasonEnum.SPRING}, 80));
+        stock.put("Backpack Upgrade", new ShopItem("Backpack Upgrade", 2000, SeasonEnum.values(), 5));
         return new Shop("Pierre", 9, 17, stock);
     }
 
     public static Shop createCarpenter() {
-        return null;
+        Map<String, ShopItem> stock = new HashMap<>();
+        stock.put("Wood", new ShopItem("Wood", 10, SeasonEnum.values(), 999));
+        stock.put("Stone", new ShopItem("Stone", 20, SeasonEnum.values(), 999));
+        stock.put("Barn", new ShopItem("Barn", 6000, SeasonEnum.values(), 2));
+        stock.put("Coop", new ShopItem("Coop", 4000, SeasonEnum.values(), 2));
+        return new Shop("Carpenter", 9, 20, stock);
     }
 
     public static Shop createFishShop() {
-        return null;
+        Map<String, ShopItem> stock = new HashMap<>();
+        stock.put("Fishing Rod", new ShopItem("Fishing Rod", 500, SeasonEnum.values(), 10));
+        stock.put("Bait", new ShopItem("Bait", 5, SeasonEnum.values(), 200));
+        return new Shop("FishShop", 9, 17, stock);
     }
 
     public static Shop createMarnie() {
-        return null;
+        Map<String, ShopItem> stock = new HashMap<>();
+        stock.put("Chicken", new ShopItem("Chicken", 800, SeasonEnum.values(), 5));
+        stock.put("Cow", new ShopItem("Cow", 1500, SeasonEnum.values(), 3));
+        stock.put("Animal Feed", new ShopItem("Animal Feed", 50, SeasonEnum.values(), 100));
+        return new Shop("Marnie", 9, 16, stock);
     }
 
     public static Shop createSaloon() {
-        return null;
+        Map<String, ShopItem> stock = new HashMap<>();
+        stock.put("Beer", new ShopItem("Beer", 300, SeasonEnum.values(), 20));
+        stock.put("Spaghetti", new ShopItem("Spaghetti", 240, SeasonEnum.values(), 15));
+        stock.put("Pizza", new ShopItem("Pizza", 300, SeasonEnum.values(), 10));
+        return new Shop("Saloon", 12, 24, stock);
     }
-    // similarly createCarpenter, createFishShop, createMarnie, createSaloon...
 }
